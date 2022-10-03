97cbbb9a2ed25552a9528f29d9a46d7ae483fe1a
Asurin219 <824907403@qq.com>
2022-10-03 12:21:39 +0800

    Site updated: 2022-10-03 12:21:30

---
+++ js/crash_cheat.js
@@ -0,0 +1,15 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/joke.ico");
+        document.title = '看不见我🙈~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = ' ( ๑•̀ㅂ•́) ✧呀被发现了～';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
